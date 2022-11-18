ed9d33cea98cc8a6ae80db386f2d5181a3564080
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2022-11-18 15:17:00 -0200

    Update stripe.ts

--- src/pages/api/stripe.ts
+++ src/pages/api/stripe.ts
@@ -1,9 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import Stripe from 'stripe'
 
-import { ICart } from "@interfaces/interfaces"
+import { ICart } from '../interfaces'
 
-// @ts-ignore
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
