53ac8994dedafdf9961f1543eb8044638ce15198
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2022-11-18 12:31:05 -0200

    Create stripe-checkout.ts

---
+++ src/pages/api/stripe-checkout.ts
@@ -0,0 +1,22 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import Stripe from 'stripe'
+
+const stripe = new Stripe('sk_test_51LLB0DLGZYTsJVoIhla5KpEFAFYx7eNkiwEJMtO3LAxvrbnoiMDSimrPyMpq5iy0UdiD2VSiQtTCTaTkJny7488E00bAXOTnbT')
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "POST") {
+    try {
+      const session = await stripe.checkout.sessions.retrieve(req.body.session_id, {
+        expand: ["line_items"],
+      })
+
+      res.status(200).json(session)
+    } catch (err: any) {
+      console.log(err)
+      res.status(err.statusCode || 500).json(err.message)
+    }
+  } else {
+    res.setHeader("Allow", "POST")
+    res.status(405).end("Method Not Allowed")
+  }
+}
