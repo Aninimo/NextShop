ac766b5cab53bb9c79358bd30af73f2be48d6dc3
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2022-11-18 12:21:30 -0200

    Create index.tsx

---
+++ src/interfaces/index.tsx
@@ -0,0 +1,25 @@
+export interface IProducts {
+  slug: string
+  id: string
+  image: {
+    url: string
+  }
+  price: number
+  quantity: number
+  title: string
+}
+
+export interface IProductInCart {
+  id: string
+  name: string
+  image: {
+    url: string
+  }
+  price: number
+  quantity: number
+}
+
+export interface ICart {
+  product: IProductInCart
+  quantity: number
+}
